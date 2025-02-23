4f3d49f9803b699ca0a6896e7d2998c4868c470f
junwoo <kjw4523705@naver.com>
2025-02-23 21:14:06 +0900

    글로벌 레이아웃 설정하기
    global-layout.tsx : _app.tsx에서 랜더링 할 게 많아지니까 또다른 global컴포넌트를 통해 랜더링. children을 _app.tsx로부터 받아서 <Component {...pageProps}/>를 랜더링
    
    style > global.css : html, body 설정
    
    global-layout.module.css : 글로벌 레이아웃의 css설정함.

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,27 +1,13 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import Link from "next/link";
+
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  
-  const onClickButton = () =>{
-    router.push("/test");
-  };
   
   return (
-    <div>
-      <Link href={"/"}>index</Link>
-      &nbsp;
-      <Link href={"/search"}>search</Link>
-      &nbsp;
-      <Link href={"/book/1"}>book</Link>
-      &nbsp;
-      <div>
-        <button onClick={onClickButton}>/test 페이지 이동</button>
-      </div>
-      <Component {...pageProps} />  
-    </div>
-
+    <GlobalLayout>
+      {/* children으로 넘기기 */}
+      <Component {...pageProps}/>
+    </GlobalLayout>
   )
 }
