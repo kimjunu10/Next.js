3d42c730fc725cc1880a4919932e38b8ed7db474
junwoo <kjw4523705@naver.com>
2025-02-23 19:05:52 +0900

    페이지 네비게이팅
    Link :  index, search, book 페이지 이동하는 링크 생성
    Programmativc Navigation : 버튼 클릭 -> 이벤트 핸들러 함수를 통해 /test로 이동동

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { useRouter } from "next/router";
+import Link from "next/link";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  
+  const onClickButton = () =>{
+    router.push("/test");
+  };
+  
+  return (
+    <div>
+      <Link href={"/"}>index</Link>
+      &nbsp;
+      <Link href={"/search"}>search</Link>
+      &nbsp;
+      <Link href={"/book/1"}>book</Link>
+      &nbsp;
+      <div>
+        <button onClick={onClickButton}>/test 페이지 이동</button>
+      </div>
+      <Component {...pageProps} />  
+    </div>
+
+  )
 }
